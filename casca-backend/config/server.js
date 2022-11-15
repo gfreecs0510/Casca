f710d27d50e1b0fda7bdbe444845a71d67ae476f
al@meta.how <al@meta.how>
2022-11-15 05:43:46 +0000

    strapi
    initial strapi source code for the backend

---
+++ casca-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8b37044c3964f1652fd095497a1a3546'),
+    },
+  },
+});
---
+++ casca-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5049056b-e6da-4719-93f7-a3ded17d7439'
+};
